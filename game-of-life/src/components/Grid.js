45eb1af7192399bb6f831a97b4cc5ce2078aadcd
Chris Giroux <cgiroux86@gmail.com>
2020-07-26 15:58:36 -0400

    grid setup completely

--- game-of-life/src/components/Grid.js
+++ game-of-life/src/components/Grid.js
@@ -1,8 +1,34 @@
 import React, { useState } from "react";
-
+import shortid from "shortid";
 const initialRows = new Array(50).fill(0);
 const initialGrid = new Array(50).fill(initialRows);
+
 export default function Grid() {
   const [grid, setGrid] = useState(initialGrid);
-  return <div>{console.log(initialGrid)}</div>;
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: `repeat(${initialRows.length}, 20px)`,
+      }}
+    >
+      {grid.map((rows) =>
+        rows.map((col) => {
+          {
+            console.log(rows, col);
+          }
+          return (
+            <div
+              key={shortid.generate()}
+              style={{
+                width: "20px",
+                height: "20px",
+                border: "1px solid black",
+              }}
+            ></div>
+          );
+        })
+      )}
+    </div>
+  );
 }
